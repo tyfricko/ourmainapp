5c9a9a1860773788ba38f0a3b7522a8e4aeaf453
Matej <zlatic.matej@gmail.com>
2023-11-03 11:33:30 +0100

    Insert pagionation for homepage post feed and minor improvements

--- resources/views/homepage-feed.blade.php
+++ resources/views/homepage-feed.blade.php
@@ -5,10 +5,7 @@
       <h2 class="text-center mv-4">The last from those you follow</h2>
         <div class="list-group">
           @foreach ($posts as $post)
-              <a href="/post/{{$post->id}}" class="list-group-item list-group-item-action">
-              <img class="avatar-tiny" src="{{$post->user->avatar}}" />
-              <strong>{{$post->title}}</strong> <span class="text-muted small">by {{$post->user->username}} on {{$post->created_at->format('m/d/Y')}}</span>
-              </a>
+            <x-post :post="$post" />
           @endforeach
         </div>
 
